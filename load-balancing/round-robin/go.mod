61ddbf48bab221358cab01c81560f5ffb192ff9c
Matheus Fidelis <matheus@nanoshots.com.br>
2023-12-16 13:02:03 -0300

    Load Balancing: RoundRobin

---
+++ load-balancing/round-robin/go.mod
@@ -0,0 +1,3 @@
+module round_robin
+
+go 1.21.4
---
+++ load-balancing/round-robin/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Abstração de um Mecanismo de Round Robin
+type RoundRobin struct {
+	hosts        []string      // Lista de Hosts disponíveis para balanceamento
+	index        int           // Index de controle
+	mutex        sync.Mutex    // Mutex para lock do index
+	quantum      time.Duration // Variável de Tempo "Quantum"
+	ultimoAcesso time.Time     // TImestamp do ultimo acesso para calcular a diferença do Quantum para troca de hosts
+}
+
+// Retorna o host ativo no momento
+func (rb *RoundRobin) getHost() string {
+
+	// Trava e destrava a seleção de hosts
+	rb.mutex.Lock()
+	defer rb.mutex.Unlock()
+
+	// Verifica se o timestamp atual é menor do que o ultimo somado com o quantum
+	// Caso seja isso significa que o host no index ainda é o ativo
+	if time.Now().Before(rb.ultimoAcesso.Add(rb.quantum)) {
+		return rb.hosts[rb.index]
+	}
+
+	// Caso o timestamp seja maior, atualiza o ultimo acesso e avança para o proximo host
+	rb.ultimoAcesso = time.Now()
+	rb.index = (rb.index + 1) % len(rb.hosts)
+
+	return rb.hosts[rb.index]
+}
+
+func main() {
+	// Variável de tempo em milisegundos pra escolha de hosts
+	var quantum int = 300
+
+	// Lista de hosts disponíveis
+	hosts := []string{
+		"http://host1.com",
+		"http://host2.com",
+		"http://host3.com",
+	}
+
+	// Inicia o mecanismo de Round Robin
+	roundRobin := RoundRobin{
+		hosts:   hosts,
+		quantum: time.Millisecond * time.Duration(quantum),
+	}
+
+	// Simula 100 Requests
+	for i := 0; i < 100; i++ {
+		host := roundRobin.getHost()
+		fmt.Printf("Requisição %d direcionada para: %s\n", i+1, host)
+		time.Sleep(100 * time.Millisecond)
+	}
+}

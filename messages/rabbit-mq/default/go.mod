06ff08bd782a227f4ec21ca39c7bcc571ebf755e
Matheus Fidelis <matheus@nanoshots.com.br>
2024-04-12 18:56:42 -0300

    DRAFT: default exchange

---
+++ messages/rabbit-mq/default/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.21.4
+
+require github.com/streadway/amqp v1.1.0
---
+++ messages/rabbit-mq/default/producer.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+// Exemplo de implementação de uma default exchange
+func main() {
+	conn, err := amqp.Dial("amqp://user:password@localhost:5672/")
+	if err != nil {
+		fmt.Println("Falha ao conectar com o broker", err)
+		return
+	}
+	defer conn.Close()
+
+	// Criando um canal
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Falha ao abrir um canal com o broker", err)
+		return
+	}
+	defer ch.Close()
+
+	// Criação da Exchange
+	err = ch.ExchangeDeclare(
+		"ecommerce.vendas.nova.venda", // Nome da exchange
+		"direct",                      // Tipo da exchange
+		true,                          // durable
+		false,                         // auto-deleted
+		false,                         // internal
+		false,                         // no-wait
+		nil,                           // arguments
+	)
+	if err != nil {
+		fmt.Println("Falha ao construir a exchange", err)
+		return
+	}
+
+	// Criação de uma Queue
+	q, err := ch.QueueDeclare(
+		"cobrar", // Nome da fila
+		true,     // durable
+		false,    // delete when unused
+		false,    // exclusive
+		false,    // no-wait
+		nil,      // arguments
+	)
+	if err != nil {
+		fmt.Println("Falha ao criar a queue", err)
+		return
+	}
+
+	// Associando a Queue até a Exchange
+	// e informando a binding key para roteamento
+	err = ch.QueueBind(
+		q.Name,                        // Nome da fila
+		"cobrar",                      // Binding key de roteamento
+		"ecommerce.vendas.nova.venda", // Nome da exchange
+		false,                         // no-wait
+		nil,                           // arguments
+	)
+
+	for i := 0; i < 30; i++ {
+
+		// Mensagem simples
+		body := fmt.Sprintf("venda:id:%v", i)
+
+		// Publicando a mensagem na exchange
+		err = ch.Publish(
+			"ecommerce.vendas.nova.venda", // exchange
+			"cobrar",                      // routing key (binding key)
+			false,                         // mandatory
+			false,                         // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		if err != nil {
+			fmt.Println("Falha ao publicar a mensagem na exchange", err)
+		}
+
+		fmt.Printf("Mensagem de venda enviada para a exchange ecommerce.vendas.nova.venda: %v\n", body)
+	}
+
+}
